4b659c06bc92aa1e5419aac5a1bd1a9ee325cd16
Hannah Warmbier <hannahwarmbier@gmail.com>
2025-03-03 11:24:33 -0600

    ✨ Add PowerShell profile

---
+++ pwsh/profile.ps1
@@ -0,0 +1,14 @@
+Set-Alias -Name php -Value $HOME\.config\herd\bin\php84\php.exe
+Set-Alias -Name touch -Value New-Item
+Set-Alias -Name which -Value where.exe
+Set-Alias -Name np -Value C:\Windows\Notepad.exe
+
+function runArtisanCommand {& php artisan $args}
+Set-Alias artisan runArtisanCommand
+Set-Alias art runArtisanCommand
+
+function runPleaseCommand {& php please $args}
+Set-Alias please runPleaseCommand
+
+$starship = "C:\Program Files\starship\bin\starship.exe"
+Invoke-Expression (&$starship init powershell)
