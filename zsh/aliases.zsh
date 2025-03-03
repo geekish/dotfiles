c62a9d7913c56701d0f81fa85c53e88396180411
Hannah Warmbier <hannahwarmbier@gmail.com>
2025-03-03 11:14:39 -0600

    ✨ Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,9 +3,10 @@ function cdl { cd "$1" && ls -aG; }
 alias chmod="chmod -Rv"
 alias chown="chown -Rv"
 alias chx="chmod a+x"
-alias ls="ls -aG"
+alias ls="ls -aG --color"
 alias mkdir="mkdir -p"
 alias rmdir="rm -rf" # don't be stupid
+alias rmzi="rm -rf **/*Zone.Identifier" # thanks Windows
 
 # PHP: COMPOSER/LARAVEL
 alias sail="[ -f sail ] && sh sail || sh vendor/bin/sail"
@@ -21,8 +22,10 @@ alias please="php please"
 
 # MISC
 alias shadcn="pnpm dlx shadcn@latest"
+alias shadcn-canary="pnpm dlx shadcn@canary"
 alias shadcn-vue="pnpm dlx shadcn-vue@latest"
 alias create-react-router="pnpm dlx create-react-router@latest"
+alias create-turbo="pnpm dlx create-turbo@latest"
 
 if [[ $(systemd-detect-virt) == "wsl" ]]; then
   function code () {
