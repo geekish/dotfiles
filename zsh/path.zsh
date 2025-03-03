c7ce8092348969d0588a14432ac933eda940134a
Hannah Warmbier <hannahwarmbier@gmail.com>
2025-03-03 11:17:12 -0600

    ✨ Update path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,11 +1,11 @@
 export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$HOME/.composer/vendor/bin:$PATH"
-export PATH="$HOME/.local/share/fnm:$PATH"
 export PATH="/usr/local/opt/php/bin:$PATH"
 export PATH="/usr/local/opt/php/sbin:$PATH"
 export PATH="/usr/local/go/bin:$PATH"
 export PATH="$HOME/.asdf/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 if [[ -d ~/$HOME/.fly ]]; then
   export FLYCTL_INSTALL="$HOME/.fly"
