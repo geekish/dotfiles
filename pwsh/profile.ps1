02d9c9dd97227f2714d351799731bc43e866b01b
Hannah Chartier <hannah@sent.com>
2025-03-18 12:43:41 -0500

    🪟 Cross-OS PowerShell profile update

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,10 +1,14 @@
-$rustbin = "$HOME\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin"
-$env:Path += ";$rustbin"
 
-Set-Alias -Name php -Value $HOME\.config\herd\bin\php84\php.exe
+if ($IsWindows) {
+  $rustbin = "$HOME\.rustup\toolchains\stable-x86_64-pc-windows-msvc\bin"
+  $env:Path += ";$rustbin"
+
+  Set-Alias -Name php -Value $HOME\.config\herd\bin\php84\php.exe
+  Set-Alias -Name which -Value where.exe
+  Set-Alias -Name np -Value C:\Windows\Notepad.exe
+}
+
 Set-Alias -Name touch -Value New-Item
-Set-Alias -Name which -Value where.exe
-Set-Alias -Name np -Value C:\Windows\Notepad.exe
 
 function runArtisanCommand {& php artisan $args}
 Set-Alias artisan runArtisanCommand
@@ -13,5 +17,5 @@ Set-Alias art runArtisanCommand
 function runPleaseCommand {& php please $args}
 Set-Alias please runPleaseCommand
 
-$starship = "C:\Program Files\starship\bin\starship.exe"
+$starship = $IsWindows ? "C:\Program Files\starship\bin\starship.exe" : "/usr/local/bin/starship"
 Invoke-Expression (&$starship init powershell)
