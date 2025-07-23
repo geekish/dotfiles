cbe5f89aacbdefa04d0408e6c2e819509a2d02e1
Hannah Chartier <hannah@sent.com>
2025-07-23 10:50:40 -0500

    🛣️ Add linuxbrew sbin to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,11 +1,11 @@
 export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 export PATH="$HOME/.config/composer/vendor/bin:$PATH"
 export PATH="/usr/local/opt/php/bin:$PATH"
 export PATH="/usr/local/opt/php/sbin:$PATH"
 export PATH="/usr/local/go/bin:$PATH"
-export PATH="$HOME/.asdf/bin:$PATH"
+export PATH="/home/hannah/.lando/bin:$PATH"; #landopath
 export PATH="$HOME/.local/bin:$PATH"
 
 if [[ -d ~/$HOME/.fly ]]; then
